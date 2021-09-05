8a1579e2ed5f0f8fc7821b655a808a6fcdbc2968
Vivek Rajagopal <vivrajagopal@gmail.com>
2021-09-06 09:43:38 +1000

    Use gatsby-ssr to add darkmode script to HTML

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,27 +1,11 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { darkmodeClassname, darkmodeSettingName } from "../utils/darkmode";
 import { Navbar } from "./Navbar";
-
-const darkmodeScriptRaw = `
-(function() {
-  if (localStorage.getItem("${darkmodeSettingName}") === "true") {
-    document.body.classList.add("${darkmodeClassname}");
-  }
-})();
-`;
 
 export const Page = ({ children, className }: { children: React.ReactNode; className?: string }) => {
   return (
     <>
-      <Helmet
-        script={[
-          {
-            type: "text/javascript",
-            innerHTML: darkmodeScriptRaw
-          }
-        ]}
-      >
+      <Helmet>
         <title>Vivek Rajagopal</title>
       </Helmet>
       <main className={className}>
